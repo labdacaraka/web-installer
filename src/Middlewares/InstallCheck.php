a146dd21c45f503219bdd57614221e1f50ac50af
Rizky Septiawan <rizseptiawan@gmail.com>
2023-03-22 20:29:23 +0700

    Fix loop redirect on InstallCheck middleware

--- src/Middlewares/InstallCheck.php
+++ src/Middlewares/InstallCheck.php
@@ -8,6 +8,9 @@ class InstallCheck
 {
     public function handle(Request $request, \Closure $next)
     {
+        if($request->is('install') || $request->is('install/*')){
+            return $next($request);
+        }
         $envatoUsername = config('web-installer.marketplace.envato.username');
         $purchaseCode = config('web-installer.marketplace.envato.purchase_code');
         $itemId = config('web-installer.marketplace.envato.item_id');

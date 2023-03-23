a3be347c229387beacae8ab7c2c91fb822bfbc12
rizkyseptiawan <rizkyseptiawan@users.noreply.github.com>
2023-03-23 00:35:30 +0000

    Fix styling

--- src/Controllers/InstallerController.php
+++ src/Controllers/InstallerController.php
@@ -98,11 +98,11 @@ class InstallerController extends Controller
 
     private function convertToBytes($val): int|string
     {
-        if(is_numeric($val)) {
+        if (is_numeric($val)) {
             return $val;
         }
         $val = trim($val);
-        $last = strtolower($val[strlen($val)-1]);
+        $last = strtolower($val[strlen($val) - 1]);
         $val = substr($val, 0, -1);
         switch($last) {
             case 'g':
